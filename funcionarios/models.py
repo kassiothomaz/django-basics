2c2f1077b6dec34ef19d9c8be333843dac104f65
kassiothomaz <k.thomaz@live.com>
2025-12-09 20:21:38 -0300

    kassiooooooo

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -31,7 +31,9 @@ class Cliente(Pessoa):
 
 class Kassio(Pessoa):
     end=models.TextField(max_length=1000)
-    telefone=models.CharField(max_length=20)
+    
+
+    
     meta = models.BigIntegerField()
     def __str__(self):
         return self.nome
